3b927ca2985653bc9170093ab74a6101f0461843
Pyatta Bhavani <pyattabhavani@gmail.com>
2026-07-23 18:21:19 +0530

    Added Week-07 Git Hands-On commands

--- Week-05-ReactJS/cricketapp/src/App.js
+++ Week-05-ReactJS/cricketapp/src/App.js
@@ -1,63 +1,15 @@
 import React from "react";
-import office from "./office.jpg";
+import ListofPlayers from "./ListofPlayers";
+import IndianPlayers from "./IndianPlayers";
 
 function App() {
 
-  const offices = [
-    {
-      name: "Smart Office",
-      rent: 55000,
-      address: "Hyderabad"
-    },
-    {
-      name: "Tech Park",
-      rent: 75000,
-      address: "Bangalore"
-    },
-    {
-      name: "Business Hub",
-      rent: 62000,
-      address: "Chennai"
-    }
-  ];
+  const flag = true;
 
-  return (
-    <div style={{ padding: "20px" }}>
+  if(flag)
+    return <ListofPlayers />;
 
-      <h1>Office Space Rental App</h1>
-
-      <img
-        src={office}
-        alt="Office"
-        width="400"
-      />
-
-      <hr />
-
-      {offices.map((item, index) => (
-
-        <div key={index} style={{ marginBottom: "20px" }}>
-
-          <h2>{item.name}</h2>
-
-          <p>
-            <b>Address:</b> {item.address}
-          </p>
-
-          <p
-            style={{
-              color: item.rent < 60000 ? "red" : "green"
-            }}
-          >
-            <b>Rent:</b> ₹{item.rent}
-          </p>
-
-        </div>
-
-      ))}
-
-    </div>
-  );
+  return <IndianPlayers />;
 }
 
 export default App;
